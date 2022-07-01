c8c039f6ffd016d9d3f21563d16f17b996bb2b76
Imjustordinary <imnotanordinary@gmail.com>
2022-07-01 20:09:38 +0630

    [upload]added the tarot app

--- src/Tarots/EachTartot.js
+++ src/Tarots/EachTartot.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View, Image, Dimensions } from 'react-native'
-import React, { useEffect, useLayoutEffect } from 'react'
+import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import React, {useEffect, useLayoutEffect} from 'react';
 import Animated, {
   Extrapolate,
   interpolate,
@@ -11,7 +11,8 @@ import Animated, {
   withTiming,
   useAnimatedReaction,
   withDelay,
-  runOnJS
+  runOnJS,
+  Easing,
 } from 'react-native-reanimated';
 import {
   GestureHandlerRootView,
@@ -26,157 +27,247 @@ const CARD_HEIGHT = CARD_WIDTH * aspectRatio;
 const IMAGE_WIDTH = CARD_WIDTH * 0.9;
 const DURATION = 250;
 
-const EachTartot = ({source, index, top, topChangeHandler, cardsLength, shuffleBack, setTopHandler}) => {
-
-  const offset = useSharedValue({ x: 0, y: 0, z:0 });
+const EachTartot = ({
+  source,
+  index,
+  cardsLength,
+  shuffleBack,
+  title,
+  meaning,
+  vector,
+}) => {
+  const offset = useSharedValue({x: 0, y: 0, z: 0});
   const translateX = useSharedValue(0);
-  const rotateX = useSharedValue(30);
-  const setCards = useSharedValue(cardsLength.value)
-  const translateY = useSharedValue(-CARD_HEIGHT-500);
+  const rotateY = useSharedValue(0);
+  const setCards = useSharedValue(cardsLength.value);
+  const translateY = useSharedValue(-CARD_HEIGHT - 500);
   const scale = useSharedValue(1);
   const rotateZ = useSharedValue(0);
-  const delay = index * DURATION;
   const theta = -10 + Math.random() * 20;
-  // const theta = -Math.random() * 10;
-
 
   const cStyle = useAnimatedStyle(() => {
-    offset.value.z = rotateZ.value
+    offset.value.z = rotateZ.value;
+
     return {
-    transform: [
-      { perspective: 1500 },
-      { rotateX: 
-        index == cardsLength.value -1 ? withSpring('15deg') : 
-        "30deg" },
-      { translateX: translateX.value },
-      { translateY: translateY.value },
-      // { rotateY: `${rotateZ.value / 10}deg` },
-      { rotateZ:
-         index == cardsLength.value -1 ? withSpring('0deg') :
-         `${rotateZ.value}deg` },
-      { scale: scale.value },
-    ],
-  }
+      transform: [
+        {perspective: 1500},
+        {
+          rotateX:
+            index == cardsLength.value - 1 ? withSpring('15deg') : '30deg',
+        },
+        {translateX: translateX.value},
+        {translateY: translateY.value},
+        {rotateY: `${rotateY.value}deg`},
+        {
+          rotateZ:
+            index == cardsLength.value - 1
+              ? withSpring('0deg')
+              : `${rotateZ.value}deg`,
+        },
+        {scale: scale.value},
+      ],
+    };
   });
 
-  useLayoutEffect(()=>{
-    const duration = 150 * index;
-    translateY.value = withDelay(
-      duration,
-      withTiming(0));
+  const imgStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      rotateY.value,
+      [0, 90],
+      [1, 0],
+      Extrapolate.CLAMP,
+    );
+    return {
+      opacity,
+    };
+  });
 
-    rotateZ.value = withDelay(
-      duration,
-      withTiming(
-        index == cardsLength.value -1 ? 0 :
-        theta)
-    )
-  },[])
-  
+  const textStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      rotateY.value,
+      [90, 180],
+      [0, 1],
+      Extrapolate.CLAMP,
+    );
+    return {
+      opacity,
+    };
+  });
+
+  const infoTextStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotateY: '180deg',
+        },
+      ],
+    };
+  });
+
+  useLayoutEffect(() => {
+    const duration = 200 * index;
+    setTimeout(() => {
+      translateY.value = withDelay(duration, withTiming(0));
+
+      rotateZ.value = withDelay(
+        duration,
+        withTiming(index == cardsLength.value - 1 ? 0 : theta),
+      );
+    }, 400);
+  }, []);
+
   const panGestureEvent = useAnimatedGestureHandler({
     onStart: (event, context) => {
       context.translateX = translateX.value;
       context.translateY = translateY.value;
-      
     },
     onActive: (event, context) => {
       translateX.value = event.translationX + context.translateX;
       translateY.value = event.translationY + context.translateY;
-      
     },
     onEnd: () => {
-   if(translateX.value > width/4 ){
-    translateX.value = withSpring(width)
-    
-    cardsLength.value = cardsLength.value  - 1;
-    console.log('set card ' + setCards.value)
-    console.log(cardsLength.value)
-    // console.log(top)
-    if(index < 1){
-      shuffleBack.value = true;
-      console.log('shuffle pls')
-    }
-    
-   }
-   else if(translateX.value < -width/4){
-    translateX.value = withSpring(-width)
-    cardsLength.value = cardsLength.value - 1;
-    if(index < 1){
-      shuffleBack.value = true;
-    }
-   }
-    //   if (distance < CIRCLE_RADIUS + SIZE / 2) {
-    //     translateX.value = withSpring(0);
-    //     translateY.value = withSpring(0);
-    //   }
+      if (translateX.value > width / 5) {
+        translateX.value = withSpring(width);
+
+        cardsLength.value = cardsLength.value - 1;
+        if (index < 1) {
+          shuffleBack.value = true;
+        }
+      } else if (translateX.value < -width / 5) {
+        translateX.value = withSpring(-width);
+        cardsLength.value = cardsLength.value - 1;
+        if (index < 1) {
+          shuffleBack.value = true;
+        }
+      }
     },
   });
 
+  const destinyChecker = () => {
+    if (rotateY.value === 180) {
+      rotateY.value = withTiming(0, {
+        duration: 700,
+        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+      });
+    } else {
+      rotateY.value = withTiming(180, {
+        duration: 700,
+        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+      });
+    }
+  };
+
   useAnimatedReaction(
     () => shuffleBack.value,
-    (v) => {
+    v => {
       if (v) {
         const duration = 150 * index;
         cardsLength.value = setCards.value;
-        translateY.value = withDelay(
-          duration,
-          withSpring(0)
-        );
+        translateY.value = withDelay(duration, withSpring(0));
+        rotateY.value = withDelay(duration, withTiming(0, {
+          duration: 700,
+          easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+        }));
         translateX.value = withDelay(
           duration,
           withSpring(0, {}, () => {
             shuffleBack.value = false;
-          })
+          }),
         );
         rotateZ.value = withDelay(duration, withSpring(theta));
       }
-    }
+    },
   );
 
   return (
     <>
-     <Animated.View style={styles.eachContainer}>
-     <PanGestureHandler onBegan={()=>console.log('begin')} onGestureEvent={panGestureEvent}>
-          
-      <Animated.View onTouchStart={()=>console.log('run')} style={[styles.card,cStyle]}>
-      <Animated.Image  
-      resizeMode='contain'
-      style={{width: IMAGE_WIDTH,
-            height: IMAGE_WIDTH * aspectRatio,}} source={source} />
-      </Animated.View>  
-      </PanGestureHandler> 
-        </Animated.View>
+      <Animated.View style={styles.eachContainer}>
+        <PanGestureHandler
+          onFailed={destinyChecker}
+          onGestureEvent={panGestureEvent}>
+          <Animated.View style={[styles.card, cStyle]}>
+            <Animated.Image
+              resizeMode="contain"
+              style={[
+                {
+                  width: IMAGE_WIDTH,
+                  height: IMAGE_WIDTH * aspectRatio,
+                  position: 'absolute',
+                },
+                imgStyle,
+              ]}
+              source={source}
+            />
+            <Animated.View
+              style={[
+                {
+                  margin: 17,
+                  marginVertical: 25,
+                  padding: 12,
+                  paddingVertical:5,
+                  justifyContent: 'flex-start',
+                  alignItems: 'center',
+                  flex: 1,
+                  borderWidth: 2.5,
+                  borderColor: '#393E46',
+                  borderRadius: 7,
+                },
+                textStyle,
+              ]}>
+              <Animated.Image
+                resizeMode="contain"
+                style={[
+                  {width: 150, height: 150, paddingVertical: 12},
+                  infoTextStyle,
+                ]}
+                source={vector}
+              />
+              <Animated.Text
+                style={[
+                  {
+                    textAlign: 'justify',
+                    lineHeight: 17,
+                    fontWeight: '400',
+                    color: '#141E27',
+                    fontSize:13
+                  },
+                  infoTextStyle,
+                ]}>
+                {meaning}
+              </Animated.Text>
+            </Animated.View>
+          </Animated.View>
+        </PanGestureHandler>
+      </Animated.View>
     </>
-  )
-}
+  );
+};
 
-export default EachTartot
+export default EachTartot;
 
 const styles = StyleSheet.create({
-  
-  eachContainer:{
-    
-...StyleSheet.absoluteFillObject,
-justifyContent:'center',
-alignItems:'center',
+  eachContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  card:{
-  backgroundColor: "white",
-  borderRadius: 10,
-  width: CARD_WIDTH,
-  height: CARD_HEIGHT,
-  justifyContent: "center",
-  alignItems: "center",
-  borderColor:'#DDD',
-  borderWidth:1,
-  // shadowColor: "#000",
-  // shadowOffset: {
-  //   width: 0,
-  //   height: 2,
-  // },
-  // shadowOpacity: 0.25,
-  // shadowRadius: 3.84,
-  // elevation: 1,
-},
-  
-})
+  textTitle: {
+    textAlign: 'center',
+    fontWeight: '600',
+    fontSize: 20,
+    marginTop: (IMAGE_WIDTH * aspectRatio) / 7,
+    lineHeight: 17,
+    padding: 20,
+    color: 'black',
+  },
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    width: CARD_WIDTH,
+    height: CARD_HEIGHT,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#DDD',
+    borderWidth: 1,
+    backgroundColor: '#FAF5E4',
+  },
+});
--- src/Tarots/Tarots.js
+++ src/Tarots/Tarots.js
@@ -1,48 +1,85 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React,{useState} from 'react'
+import { Button, StyleSheet, View, Text } from 'react-native'
+import React,{useEffect, useState} from 'react'
 import EachTartot from './EachTartot'
-import Animated, {
-  Extrapolate,
-  interpolate,
-  interpolateColor,
-  useAnimatedGestureHandler,
-  useAnimatedStyle,
-  useSharedValue,
-  withSpring,
-  withTiming,
-  useAnimatedReaction,
-  withDelay
-} from 'react-native-reanimated';
+import {useSharedValue} from 'react-native-reanimated';
 import {
   GestureHandlerRootView,
-  PanGestureHandler,
 } from 'react-native-gesture-handler';
 
 const cards = [
   {
     source: require("./assets/death.png"),
+    vector: require("./vectors/death.png"),
+    title:'Death',
+    meaning:'Unlikely that this card actually represents a physical death. Typically it implies an end, possibly of a relationship or interest, and therefore implies an increased sense of self-awareness.'
   },
   {
     source: require("./assets/chariot.png"),
+    vector: require("./vectors/chariot.png"),
+    title:'Chariot',
+    meaning:'The Chariot is a card about overcoming conflicts and moving forward in a positive direction. One needs to keep going on and through sheer hard work and commitment he will be victorious.'
   },
   {
     source: require("./assets/high-priestess.png"),
+    vector: require("./vectors/high-priestess.png"),
+    title:'High Priestess',
+    meaning:'High Priestess is a card of mystery, stillness and passivity. This card suggests that it is time to retreat and reflect upon the situation and trust your inner instincts to guide you through it. Things around you are not what they appear to be right now.'
   },
   {
     source: require("./assets/justice.png"),
+    vector: require("./vectors/justice.png"),
+    title:'Justice',
+    meaning:'The Justice card indicates that the fairest decision will be made. Justice is the sword that cuts through a situation, and will not be swayed by outer beauty when deciding what is fair and just.'
   },
   {
     source: require("./assets/lover.png"),
+    vector: require("./vectors/lover.png"),
+    title:'Lover',
+    meaning:"The Lovers represent relationships and choices. Its appearance in a spread indicates some decision about an existing relationship, a temptation of the heart, or a choice of potential partners. Often an aspect of the querent's life will have to be sacrificed and a relationship gained (or vice versa), or one potential partner may be chosen while another is turned down."
   },
   {
     source: require("./assets/pendu.png"),
+    vector: require("./vectors/pendu.png"),
+    title:'Pendu',
+    meaning:'The Hanged Man is the card that suggests ultimate surrender, sacrifice, or being suspended in time.'
   },
   {
     source: require("./assets/tower.png"),
+    vector: require("./vectors/tower.png"),
+    title:'Tower',
+    meaning:'The Tower is commonly interpreted as meaning danger, crisis, destruction, and liberation. It is associated with sudden unforseen change.'
   },
   {
     source: require("./assets/strength.png"),
+    vector: require("./vectors/strength.png"),
+    title:'Strength',
+    meaning:'Strength predicts the triumphant conclusion to a major life problem, situation or temptation through strength of character. It is a very happy card if you are fighting illness or recovering from injury.'
   },
+  {
+    source: require("./assets/devil.png"),
+    vector: require("./vectors/devil.png"),
+    title:'Devil',
+    meaning:'It represents being seduced by the material world and physical pleasures. Also living in fear, domination and bondage, being caged by an overabundance of luxury, discretion should be used in personal and business matters.'
+  },
+  {
+    source: require("./assets/fool.png"),
+    vector: require("./vectors/fool.png"),
+    title:'Fool',
+    meaning:"The Fool represents new beginnings, having faith in the future, being inexperienced, not knowing what to expect, having beginner's luck, improvisation and believing in the universe."
+  },
+  {
+    source: require("./assets/judegment.png"),
+    vector: require("./vectors/judgement.png"),
+    title:'Judegment',
+    meaning:'This card is referred to as a time of resurrection and awakening, a time when a period of our life comes to an absolute end making way for dynamic new beginnings.'
+  },
+  {
+    source: require("./assets/moon.png"),
+    vector: require("./vectors/moon.png"),
+    title:'Moon',
+    meaning:'The Moon is a card of illusion and deception, and therefore often suggests a time when something is not as it appears to be. Perhaps a misunderstanding on your part, or a truth you cannot admit to yourself.'
+  },
+
 ];
 
 const Tarots = () => {
@@ -50,6 +87,7 @@ const Tarots = () => {
   const cardsLength = useSharedValue(cards.length)
 
   const [top, setTop] = useState(cards.length)
+  const [shuffledCards, setShuffledCards] = useState()
 
   const topChangeHandler =()=>{
     setTop(prev=> prev - 1)
@@ -59,11 +97,41 @@ const Tarots = () => {
     setTop(cards.length)
   }
 
+  const shuffle=(array)=> {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
+
+
+  useEffect(()=>{
+    shuffle(cards)
+    // console.log(cards)
+    setShuffledCards([...cards])
+  },[])
 
   return (
     <View style={styles.container}>
-      <GestureHandlerRootView style={{flex: 1}}>
-      {cards.map((each, index)=><EachTartot key={index} cardsLength={cardsLength} shuffleBack={shuffleBack} setTopHandler={setTopHandler} topChangeHandler={topChangeHandler} top={top} index={index} source={each.source} />)}
+      <GestureHandlerRootView style={{flex: 1, justifyContent:'flex-end'}}>
+      {shuffledCards && shuffledCards.map((each, index)=><EachTartot key={index} cardsLength={cardsLength} shuffleBack={shuffleBack} vector={each.vector} index={index} source={each.source} title={each.title} meaning={each.meaning} />)}
+      {/* <View>
+          <Text>
+            Find my destiny
+          </Text>
+      </View> */}
       </GestureHandlerRootView>
     </View>
   )
